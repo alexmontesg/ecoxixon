ce19dc8a6700a04069fba709e07a6568a70dc87f
alexmontesg <alejandro.montes.g@gmail.com>
2013-10-25 21:51:16 +0200

    Adds some info of events
    Not complete yet

--- eventDiscovery.php
+++ eventDiscovery.php
@@ -7,6 +7,7 @@ $db = new mysqli($db_host, $db_user, $db_pass, $db_name) or die('error with conn
 if (isset($_POST['load_url'])) {
 	$dom = new DOMDocument;
 	$dom -> loadXML(file_get_contents($_POST['data_url']));
+	$_SESSION['xml'] = $dom;
 	$categories = $dom -> getElementsByTagName('categoria');
 	$catArr = array();
 	$catInDB = array();
@@ -19,6 +20,9 @@ if (isset($_POST['load_url'])) {
 			array_push($catArr, $category -> nodeValue);
 		}
 	}
+	if(empty($catArr)) {
+		addEvents($db);
+	}
 } else if (isset($_POST['add_categories'])) {
 	$i = 0;
 	foreach ($_POST['category'] as $category) {
@@ -26,6 +30,20 @@ if (isset($_POST['load_url'])) {
 		$category = $db -> real_escape_string($category);
 		$ecoQuality = $db -> real_escape_string($ecoQuality);
 		$query = $db -> query("INSERT INTO categorias (nombre, puntuacion) VALUES ('$category', '$ecoQuality')");
+	}
+	addEvents($db);
+}
+
+function addEvents($db) {
+	$events = $_SESSION['xml'] -> getElementsByTagName('evento');
+	foreach($events as $event) {
+		$name = $db -> real_escape_string(strip_tags($event -> getElementsByTagName('nombre') -> item(0) -> nodeValue));
+		$dateStart = $db -> real_escape_string(strip_tags($event -> getElementsByTagName('dia-de-inicio') -> item(0) -> nodeValue));
+		$dateEnd = $db -> real_escape_string(strip_tags($event -> getElementsByTagName('dia-de-fin') -> item(0) -> nodeValue));
+		$description = $db -> real_escape_string(strip_tags($event -> getElementsByTagName('descripcion') -> item(0) -> nodeValue));
+		$localization = $db -> real_escape_string(strip_tags($event -> getElementsByTagName('localizacion') -> item(0) -> nodeValue));
+		$db -> query("INSERT INTO eventos (nombre, descripcion, inicio, fin, localizacion) VALUES ('$name', '$description', '$dateStart', '$dateEnd', '$localization')");
+		// TODO Calculate ecoQuality, update event with ecoQuality and add to eventos_categorias table
 	}
 }
 ?>
@@ -40,7 +58,7 @@ if (isset($_POST['load_url'])) {
 			<input type="url" name="data_url" value="http://datos.gijon.es/doc/informacion/eventos.xml"/>
 			<input type="submit" name="load_url" value="Cargar"/>
 		</form>
-		<?php if(isset($catArr)): ?>
+		<?php if(isset($catArr) && !empty($catArr)): ?>
 		<h2>Categorías por añadir</h2>
 			<form method="POST">
 				<?php foreach ($catArr as $cat):?>
